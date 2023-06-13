5f676c1ddcca09fe04ab44b45b02490d4e3b5068
Riantix <changjukim24@gmail.com>
2023-06-13 11:34:39 -0400

    Edited file structure and added common functions

---
+++ AoC2022/02 Rock Paper Scissors/Part1/StrategyGuide.java
@@ -0,0 +1,10 @@
+import AoC2022.Common.ParseInput;
+
+public class StrategyGuide {
+
+    public static void main(String[] args) {
+        
+        String s = "123";
+        s = ParseInput.identity(s);  
+    }
+}
---
+++ AoC2022/Common/ParseInput.java
@@ -0,0 +1,30 @@
+package AoC2022.Common;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ParseInput {
+    
+    public static String identity(String s) {
+        return s;
+    }
+
+    public static String[] parseInput(String fileName) {
+        String[] linesArray = new String[0];
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            ArrayList<String> lines = new ArrayList<String>();
+            
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+
+            linesArray = lines.toArray(new String[0]);
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found: " + fileName);
+        }
+        return linesArray;
+    }
+}
