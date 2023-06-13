7f4f2b63514aa4f37d75cbf0d50f0b00234239e9
Riantix <changjukim24@gmail.com>
2023-06-13 13:02:12 -0400

    Edited code for better standards

--- advent_of_code_2022/problem_02/part1/StrategyGuide.java
+++ advent_of_code_2022/problem_02/part1/StrategyGuide.java
@@ -7,23 +7,33 @@ import java.util.Set;
 
 public class StrategyGuide {
     static final String INPUT_PATH = "advent_of_code_2022/problem_02/input.txt";
-    static final Set<Character> opponentChoices = Set.of('A', 'B', 'C');
-    static final Map<Character, Integer> myChoices = Map.of('X', 1, 'Y', 2, 'Z', 3);
+    static final Set<Character> OPPONENT_CHOICES = Set.of('A', 'B', 'C');
+    static final Map<Character, Integer> MY_CHOICES = Map.of('X', 1, 'Y', 2, 'Z', 3);
 
 
     public static void main(String[] args) {
         String[] inputArray = ParseInput.parseInput(new File(INPUT_PATH));
-        int score = findTotalScore(inputArray);
+        int score = 0;
+        try {
+            score = findTotalScore(inputArray);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
         System.out.println("Expected total score: " + score);
     }
 
+    /**
+     * Finds the total score of several games
+     * 
+     * @param input a list of rock-paper-scissors games
+     * @return the total score of the games
+     */
     public static int findTotalScore(String[] input) {
         int totalScore = 0;
         for (int i = 0; i < input.length; i++) {
             String line = input[i];
             if (!validateInputLine(line)) {
-                System.out.println("Error: invalid format in line #" + i + ": " + line);
-                return -1;
+                throw new IllegalArgumentException("Invalid format in line #" + i + ": " + line);
             }
             char opponentAction = line.charAt(0);
             char myAction = line.charAt(2);
@@ -33,17 +43,31 @@ public class StrategyGuide {
         return totalScore;
     }
 
-    // Check if line is in the format of "p q" where p = {A, B, C} and q = {X, Y, Z}
+    /**
+     * Validates a single game
+     * 
+     * @param line a single rock-paper-scissors game
+     * @return true if the line is in a "p q" format, where p = {A, B, C} and q = {X, Y, Z}
+     */
     private static boolean validateInputLine(String line) {
         return
             line.length() == 3 &&
-            opponentChoices.contains(line.charAt(0)) &&
+            OPPONENT_CHOICES.contains(line.charAt(0)) &&
             line.charAt(1) == ' ' &&
-            myChoices.containsKey(line.charAt(2));
+            MY_CHOICES.containsKey(line.charAt(2));
     }
 
+    /**
+     * Calculates the expected score of a single game of rock-paper-scissors.
+     * The score is determined by 6 points for a win, and 3 points for a draw.
+     * In addition, the player gets 1 point for rock, 2 points for paper and 3 points for scissors.
+     * 
+     * @param opponentAction the opponent's action ('A', 'B', or 'C')
+     * @param myAction the player's action ('X', 'Y', 'Z')
+     * @return the calculated score based on the actions
+     */
     private static int calculateScore(char opponentAction, char myAction) {
-        int totalScore = myChoices.get(myAction);
+        int totalScore = MY_CHOICES.get(myAction);
         // My win
         if (opponentAction == 'A' && myAction == 'Y' ||
             opponentAction == 'B' && myAction == 'Z' || 
