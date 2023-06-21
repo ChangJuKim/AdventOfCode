5dc3da5372a9b89e305076324dfe36e4eda41fda
Riantix <changjukim24@gmail.com>
2023-06-21 10:55:05 -0400

    Added comments and made changes

--- advent_of_code_2022/problem_06/part1/DistinctFourSubstring.java
+++ advent_of_code_2022/problem_06/part1/DistinctFourSubstring.java
@@ -15,17 +15,23 @@ public class DistinctFourSubstring {
         System.out.println("You will find a substring length " + SUBSTRING_LENGTH + " with all distinct characters after " + countCharsUntilDistinctFourSubstring(input) + " characters");
     }
 
+    /**
+     * Finds the first instance of a SUBSTRING_LENGTH substring that has all distinct characters. Returns how many characters you would have
+     * to pass in the buffer (input) to reach the end of that substring.
+     * 
+     * @param buffer String and input
+     * @return The index+1 of the first instance of a desired length substring with all distinct characters.
+     */
     public static int countCharsUntilDistinctFourSubstring(String buffer) {
         int counter = 0;
         Map<Character, Integer> map = new HashMap<>();
         while (counter < buffer.length()) {
-            Character newCharacter = buffer.charAt(counter);
-            map.put(newCharacter, map.getOrDefault(newCharacter, 0) + 1);
+            addInstanceToMap(map, buffer.charAt(counter));
             if (counter >= SUBSTRING_LENGTH) {
-                Character oldCharacter = buffer.charAt(counter - SUBSTRING_LENGTH);
-                removeFromMap(map, oldCharacter);
+                removeInstanceFromMap(map, buffer.charAt(counter - SUBSTRING_LENGTH));
             }
-            if (map.size() >= 4) {
+            if (map.size() >= SUBSTRING_LENGTH) {
+                // We have a substring of all distinct characters
                 return counter + 1;
             }
             counter++;
@@ -33,7 +39,13 @@ public class DistinctFourSubstring {
         return buffer.length();
     }
 
-    private static void removeFromMap(Map<Character, Integer> map, Character c) {
+    /**
+     * Removes a count of a character from a map. If the count is 0, removes it from the map.
+     * 
+     * @param map Map to be changed
+     * @param c Character to be removed
+     */
+    private static void removeInstanceFromMap(Map<Character, Integer> map, Character c) {
         if (!map.containsKey(c)) {
             throw new IllegalArgumentException("Tried to remove a nonexistent key (" + c + ") from the map: " + map);
         }
@@ -41,5 +53,15 @@ public class DistinctFourSubstring {
         if (map.get(c) <= 0) {
             map.remove(c);
         }
+    }
+
+    /**
+     * Adds a count of a character to a map
+     * 
+     * @param map
+     * @param c
+     */
+    private static void addInstanceToMap(Map<Character, Integer> map, Character c) {
+        map.put(c, map.getOrDefault(c, 0) + 1);
     }
 }
