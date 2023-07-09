ba61af5cd5b7457b1be1ab53c909c7e8e58d2ff7
Riantix <changjukim24@gmail.com>
2023-07-09 10:52:02 -0400

    Updated to add generics

--- advent_of_code_2022/common/PrintArray.java
+++ advent_of_code_2022/common/PrintArray.java
@@ -1,7 +1,7 @@
 package advent_of_code_2022.common;
 
-public class PrintArray {
-    public static void printArray(int[] ary) {
+public class PrintArray<T> {
+    public void printArray(T[] ary) {
         if (ary.length == 0) System.out.println("[]");
         System.out.print("[");
         for (int i = 0; i < ary.length - 1; i++) {
@@ -10,16 +10,7 @@ public class PrintArray {
         System.out.println(ary[ary.length - 1] + "]");
     }
 
-    public static void printArray(Boolean[] ary) {
-        if (ary.length == 0) System.out.println("[]");
-        System.out.print("[");
-        for (int i = 0; i < ary.length - 1; i++) {
-            System.out.print(ary[i] + ", ");
-        }
-        System.out.println(ary[ary.length - 1] + "]");
-    }
-
-    public static void print2DArray(int[][] ary) {
+    public void print2DArray(T[][] ary) {
         System.out.println("[");
         for (int i = 0; i < ary.length; i++) {
             printArray(ary[i]);
