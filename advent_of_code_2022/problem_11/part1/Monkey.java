f4b17368457baaf69a5cad8a342940b57d20c6ce
Riantix <changjukim24@gmail.com>
2023-07-10 15:35:33 -0400

    Finished scrappy input parsing 11.1

--- advent_of_code_2022/problem_11/part1/Monkey.java
+++ advent_of_code_2022/problem_11/part1/Monkey.java
@@ -3,118 +3,82 @@ package advent_of_code_2022.problem_11.part1;
 import java.util.LinkedList;
 
 public class Monkey {
-    static final String OPERATOR_MULTIPLY = "MULTIPLY";
-    static final String OPERATOR_ADD = "ADD";
-    static final String OPERATOR_SUBTRACT = "SUBTRACT";
-    static final String OPERATOR_DIVIDE = "DIVIDE";
 
-
-    private final int ID;
+    private int ID;
     private LinkedList<Integer> heldObjects;
     private String operationFunction;
     private int operationNumber;
     private String testFunction;
     private int testNumber;
-    private Monkey receiverIfTrue;
-    private Monkey receiverIfFalse;
+    private int trueReceiverID;
+    private int falseReceiverID;
 
-    public Monkey(int ID) {
-        this.ID = ID;
-        heldObjects = new LinkedList<>();
+
+    public void addItem(int item) {
+        heldObjects.addLast(item);
     }
 
-    /**
-     * Adds items to the monkey's list of held items
-     * @param line Expected to be in the format "Starting itms: 13, 23, 15" where after "items:" are comma and space separated integers
-     */
-    public void setHeldObjects(String line) {
-        String START_OF_LINE = "Starting items: ";
-
-        int index = skipPastStart(line, START_OF_LINE);
-
-        String[] objects = line.substring(index).split(", ", index);
-        try {
-            for (String object : objects) {
-                heldObjects.add(Integer.parseInt(object));
-            }
-        } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("Expected numbers after \"Starting items: \" but got " + line);
-        }
-    }
-
-    // Expects line to be in the format of "Operation: new = old * 11". * can be replaced with -+/ and 11 can be any integer
-    // Sets the operationFunction and operationNumber variables
-    public void setOperation(String line) {
-        String START_OF_LINE = "Operation: new = old ";
-
-        int index = skipPastStart(line, START_OF_LINE);
-
-        char operator = line.charAt(index);
-        if (operator == '+') operationFunction = OPERATOR_ADD;
-        else if (operator == '-') operationFunction = OPERATOR_SUBTRACT;
-        else if (operator == '*') operationFunction = OPERATOR_MULTIPLY;
-        else if (operator == '/') operationFunction = OPERATOR_DIVIDE;
-        else {
-            throw new IllegalArgumentException("Invalid operator: " + line);
-        }
-        
-        index += 2;
-        operationNumber = getNumber(line, index, line.length());
-    }
-
-    // Expects testLine to be in the format of "Test: divisble by 13" where divisible should just be divisble and 13 can be any integer
-    // Expects trueLine to be in the format of "If true: throw to monkey 2" where 2 can be any integer
-    // Expects falseLine to be in the format of "If false: throw to monkey 4" where 4 can be any integer
-    // Sets testFunction, testNumber, receiverIfTrue and receiverIfFalse fields
-    public void setTestCondition(String testLine) {
-        String START_OF_TEST_LINE = "Test: divisble by ";
-
-        int index = skipPastStart(testLine, START_OF_TEST_LINE);
-        testFunction = OPERATOR_DIVIDE;
-        testNumber = getNumber(testLine, index, testLine.length());
-    }
-
-    public void setMonkey(Monkey trueMonkey, Monkey falseMonkey) {
-        receiverIfTrue = trueMonkey;
-        receiverIfFalse = falseMonkey;
-    }
-
-    public int returnDesiredMonkey(String trueOrFalseLine) {
-        String START_OF_TRUE_LINE = "If true: throw to monkey ";
-        String START_OF_FALSE_LINE = "If false: throw to monkey ";
-
-        try {
-            int index = skipPastStart(trueOrFalseLine, START_OF_TRUE_LINE);
-            return getNumber(START_OF_TRUE_LINE, index, trueOrFalseLine.length());
-        } catch (IllegalArgumentException ex) {
-        }
-
-        try {
-            int index = skipPastStart(trueOrFalseLine, START_OF_FALSE_LINE);
-            return getNumber(START_OF_FALSE_LINE, index, trueOrFalseLine.length());
-        } catch (IllegalArgumentException ex) {
-            throw ex;
-        }
+    @Override
+    public String toString() {
+        String string = "Monkey " + ID;
+        string += "\n\t Items: " + heldObjects + 
+                  "\n\t Operation: " + operationFunction + " " + operationNumber +
+                  "\n\t Test: " + testFunction + " " + testNumber +
+                  "\n\t\t If true: throw to monkey " + trueReceiverID +
+                  "\n\t\t If false: throw to monkey " + falseReceiverID + "\n";
+        return string;
     }
 
     
-    // Finds first index of startOfLine in line, and returns the index immediately after it.
-    private int skipPastStart(String line, String startOfLine) {
-        int index = line.indexOf(startOfLine);
-        if (index == -1) {
-            throw new IllegalArgumentException("Expected to begin with " + startOfLine + " but got " + line);
+
+    public static class Builder {
+        private final int ID;
+        private String operationFunction;
+        private int operationNumber;
+        private String testFunction;
+        private int testNumber;
+        private int trueReceiverID;
+        private int falseReceiverID;
+
+        public Builder(int ID) {
+            this.ID = ID;
         }
-        return index + startOfLine.length() + 1;
+
+        public Builder operationExpression(String function, int number) {
+            operationFunction = function;
+            operationNumber = number;
+            return this;
+        }
+
+        public Builder testExpression(String function, int number) {
+            testFunction = function;
+            testNumber = number;
+            return this;
+        }
+
+        public Builder trueMonkey(int trueMonkeyID) {
+            trueReceiverID = trueMonkeyID;
+            return this;
+        }
+
+        public Builder falseMonkey(int falseMonkeyID) {
+            falseReceiverID = falseMonkeyID;
+            return this;
+        }
+
+        public Monkey build() {
+            return new Monkey(this);
+        }
     }
 
-    // Returns the integer in line between start and end index. If endIndex >= line.length, then parses from start index to end of string.
-    private int getNumber(String line, int startIndex, int endIndex) {
-        try {
-            return Integer.parseInt(line.substring(startIndex, endIndex));
-        } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("Expected number between indices " + startIndex + " and " + endIndex + " of " + line);
-        }
+    public Monkey(Builder builder) {
+        this.ID = builder.ID;
+        heldObjects = new LinkedList<>();
+        operationFunction = builder.operationFunction;
+        operationNumber = builder.operationNumber;
+        testFunction = builder.testFunction;
+        testNumber = builder.testNumber;
+        trueReceiverID = builder.trueReceiverID;
+        falseReceiverID = builder.falseReceiverID;
     }
-
-
 }
--- advent_of_code_2022/problem_11/part1/SimulateMonkeyShenanigans.java
+++ advent_of_code_2022/problem_11/part1/SimulateMonkeyShenanigans.java
@@ -1,10 +1,18 @@
 package advent_of_code_2022.problem_11.part1;
 
+import java.util.ArrayList;
+
 import advent_of_code_2022.common.ParseInput;
+import advent_of_code_2022.common.PrintArray;
 
 public class SimulateMonkeyShenanigans {
     
-    static final String INPUT_FILE_NAME = "advent_of_code_2022/problem_10/input.txt";
+    static final String INPUT_FILE_NAME = "advent_of_code_2022/problem_11/input.txt";
+    static final String OPERATOR_MULTIPLY = "MULTIPLY";
+    static final String OPERATOR_ADD = "ADD";
+    static final String OPERATOR_SUBTRACT = "SUBTRACT";
+    static final String OPERATOR_DIVIDE = "DIVIDE";
+    static final String OPERATOR_EXPONENT = "EXPONENT";
     
     public static void main(String[] args) {
         String[] input = ParseInput.parseInputAsList(INPUT_FILE_NAME);
@@ -12,7 +20,114 @@ public class SimulateMonkeyShenanigans {
     }
 
     public static int productOfTopMonkeyShenanigans(String[] input) {
-        
+        Monkey[] monkeys = parseInputAndGetMonkeys(input);
+        new PrintArray<Monkey>().printArray(monkeys);
         return 0;
+    }
+
+    private static Monkey[] parseInputAndGetMonkeys(String[] input) {
+        final int NUM_LINES_DEFINING_MONKEY = 6; // Does not inclue the newline separating monkeys
+        
+        // Group input together depending on monkey
+        ArrayList<String[]> monkeyParamList = new ArrayList<>();
+        for (int i = 0; i < input.length; i += NUM_LINES_DEFINING_MONKEY + 1) {
+            String[] newMonkeyParams = new String[NUM_LINES_DEFINING_MONKEY];
+            for (int j = 0; j < newMonkeyParams.length; j++) {
+                newMonkeyParams[j] = input[i+j];
+            }
+            monkeyParamList.add(newMonkeyParams);
+        }
+
+        // Build each monkey
+        Monkey[] monkeyList = new Monkey[monkeyParamList.size()];
+        for (int i = 0; i < monkeyParamList.size(); i++) {
+            String[] monkeyParams = monkeyParamList.get(i);
+            try {
+                String idLine = monkeyParams[0];
+                int startIndex = skipPastStart(idLine, "Monkey ");
+                int id = getNumber(idLine, startIndex, idLine.indexOf(":"));
+
+                String itemLine = monkeyParams[1];
+                startIndex = skipPastStart(itemLine, "Starting items: ");
+                String[] totalItems = itemLine.substring(startIndex).split(", ");
+                int[] items = new int[totalItems.length];
+                for (int itemIndex = 0; itemIndex < totalItems.length; itemIndex++) {
+                    items[itemIndex] = Integer.parseInt(totalItems[itemIndex]);
+                }
+
+                String operationLine = monkeyParams[2];
+                startIndex = skipPastStart(operationLine, "Operation: new = old ");
+                String operatorFunction;
+                int operatorNumber;
+                // Edge case, I really cannot handle this right now.
+                if (operationLine.substring(startIndex).equals("* old")) {
+                    operatorFunction = OPERATOR_EXPONENT;
+                    operatorNumber = 2;
+                } else {
+                    operatorFunction = determineOperator(operationLine.charAt(startIndex));
+                    operatorNumber = getNumber(operationLine, startIndex + 2);
+                }
+
+                String testLine = monkeyParams[3];
+                String testFunction = determineOperator('/');
+                int testNumber = getNumber(testLine, skipPastStart(testLine, "Test: divisible by "));
+
+                String trueMonkeyLine = monkeyParams[4];
+                int trueMonkeyID = getNumber(trueMonkeyLine, skipPastStart(trueMonkeyLine, "If true: throw to monkey "));
+
+                String falseMonkeyLine = monkeyParams[5];
+                int falseMonkeyId = getNumber(falseMonkeyLine, (skipPastStart(falseMonkeyLine, "If false: throw to monkey ")));
+
+                Monkey newMonkey = new Monkey.Builder(id)
+                                             .operationExpression(operatorFunction, operatorNumber)
+                                             .testExpression(testFunction, testNumber)
+                                             .trueMonkey(trueMonkeyID)
+                                             .falseMonkey(falseMonkeyId)
+                                             .build();
+
+                for (int item : items) {
+                    newMonkey.addItem(item);
+                }
+                monkeyList[i] = newMonkey;
+            }
+            catch (NumberFormatException ex) {
+                throw new IllegalArgumentException("Tried to parse integer in line. " + ex);
+            }
+        }
+
+        return monkeyList;
+    }
+
+    private static String determineOperator(char operator) {
+        if (operator == '+') return OPERATOR_ADD;
+        else if (operator == '-') return OPERATOR_SUBTRACT;
+        else if (operator == '*') return OPERATOR_MULTIPLY;
+        else if (operator == '/') return OPERATOR_DIVIDE;
+        else {
+            throw new IllegalArgumentException("Unable to determine operator: " + operator);
+        }
+    }
+
+
+    // Finds first index of startOfLine in line, and returns the index immediately after it.
+    private static int skipPastStart(String line, String startOfLine) {
+        int index = line.indexOf(startOfLine);
+        if (index == -1) {
+            throw new IllegalArgumentException("Expected to begin with \"" + startOfLine + "\" but got \"" + line + "\"");
+        }
+        return index + startOfLine.length();
+    }
+
+    private static int getNumber(String line, int startIndex) {
+        return getNumber(line, startIndex, line.length());
+    }
+
+    // Returns the integer in line between start and end index. If endIndex >= line.length, then parses from start index to end of string.
+    private static int getNumber(String line, int startIndex, int endIndex) {
+        try {
+            return Integer.parseInt(line.substring(startIndex, endIndex));
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("Expected number between indices " + startIndex + " and " + endIndex + " of \"" + line + "\"");
+        }
     }
 }
