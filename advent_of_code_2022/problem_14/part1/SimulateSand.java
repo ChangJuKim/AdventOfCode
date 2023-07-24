1813b3053b3d6a6c7b5aad9fd42babf684f037f7
Riantix <changjukim24@gmail.com>
2023-07-24 18:08:57 -0400

    Added comments and finished 14.1

--- advent_of_code_2022/problem_14/part1/SimulateSand.java
+++ advent_of_code_2022/problem_14/part1/SimulateSand.java
@@ -19,8 +19,18 @@ public class SimulateSand {
         System.out.println(countRestingSand(input) + " pieces of sand come to a rest before falling to the void.");
     }
 
+    /**
+     * Counts the units of sand that come to rest when falling down a rock formation.
+     * This is done by using a set to identify all of the locations of the rocks and finding the size.
+     * Then the sand pieces are placed into the set (as once they're at rest, they no longer move).
+     * The resulting pieces of sand at at rest is the difference of the two sizes.
+     * 
+     * @param input The locations of the rocks
+     * @return The number of units of sand that come to a rest on top of the rocks.
+     */
     public static int countRestingSand(String[] input) {
         HashSet<Coordinate2D> blocked = new HashSet<Coordinate2D>();
+        // Parse through input to find all rocks
         for (String line : input) {
             addRocksToSet(blocked, line);
         }
@@ -28,6 +38,7 @@ public class SimulateSand {
         // For some reason, a high y value means a low point in space -- axis is reversed
         int highestPoint = findHighestYValue(blocked);
 
+        // Simulates sand trickling down the rock formation
         simulateSand(blocked, highestPoint);
         int sandAndRocks = blocked.size();
         return sandAndRocks - totalRocks;
@@ -44,6 +55,7 @@ public class SimulateSand {
         String[] lineCoordinates = line.split("->");
         if (lineCoordinates.length == 0) return;
         
+        // Parse through input to find all rocks
         Coordinate2D start = getCoordinate(lineCoordinates[0]);
         blocked.add(start);
         for (String coordinateDetails : lineCoordinates) {
@@ -63,27 +75,45 @@ public class SimulateSand {
         }
     }
 
+    /**
+     * Simulates all of the sand pieces trickling down the rocks formation. Stops once a piece of sand has no resting spot.
+     * 
+     * @param blocked The locations of the rocks.
+     * @param highestY An integer representing the lowest point of a rock. A sand below this point is considered to be falling forever.
+     */
     private static void simulateSand(HashSet<Coordinate2D> blocked, int highestY) {
         int blockedSize = blocked.size();
         do {
+            blockedSize = blocked.size();
             Coordinate2D nextSandLocation = findNextSandLocation(blocked, highestY);
-            System.out.println(nextSandLocation);
             if (nextSandLocation != null) blocked.add(nextSandLocation);
         } while (blockedSize != blocked.size());
     }
 
+    /**
+     * Returns the resulting Coordinate of the resting spot for the next piece of sand.
+     * Returns null if the sand is not caught by any rocks or sand, and falls to the void.
+     * 
+     * @param blocked A set containing all the locations of the rocks and sand. We assume these pieces cannot move.
+     * @param highestYCoord An integer indicating the lowest point of a rock. A sand below this point is effectively falling into the void.
+     * @return A Coordinate2D of the resting spot of the next sand piece. Returns null if it will fall forever.
+     */
     private static Coordinate2D findNextSandLocation(HashSet<Coordinate2D> blocked, int highestYCoord) {
         Coordinate2D sandParticle = SAND_RESIVOIR;
-        boolean foundNextSpot = false;
         while (sandParticle.getY() <= highestYCoord) {
             Coordinate2D next = null;
+            boolean foundNextSpot = false;
+            // Check straight down, down left, and then down right for a valid location
             for (Coordinate2D direction : FALLING_DIRECTION) {
                 if (!blocked.contains(sandParticle.add(direction))) {
                     next = sandParticle.add(direction);
                     foundNextSpot = true;
+                    break;
                 }
             }
-            if (foundNextSpot) sandParticle = next;
+            if (foundNextSpot) {
+                sandParticle = next;
+            }
             else return sandParticle;
         }
         // We past all the rocks, now there's just the void left
