92a91f10a44bb3d07489e4a559f7482177c1b4ef
Riantix <changjukim24@gmail.com>
2023-08-14 11:01:09 -0400

    Cleaned up code clarity

---
+++ advent_of_code_2022/problem_14/part1/ParseInputIntoRocks.java
@@ -0,0 +1,75 @@
+package advent_of_code_2022.problem_14.part1;
+
+import java.util.HashSet;
+
+import advent_of_code_2022.common.Coordinate2D;
+
+public class ParseInputIntoRocks {
+    private static HashSet<Coordinate2D> rocksSet;
+    
+    public static HashSet<Coordinate2D> getRocks(String[] input) {
+        rocksSet = new HashSet<>();
+        for (String line : input) {
+            addRocksFromLine(line);
+        }
+        return rocksSet;
+    }
+
+    private static void addRocksFromLine(String line) {
+        String[] cornerPoints = line.split("->");
+        if (line.length() == 0 || cornerPoints.length == 0) {
+            return;
+        }
+        
+        Coordinate2D start = parseAsCoordinate(cornerPoints[0]);
+        for (String coordinateDetails : cornerPoints) {
+            Coordinate2D next = parseAsCoordinate(coordinateDetails);
+            addRocksToSet(start, next);
+            start = next;
+        }
+    }
+    
+    private static Coordinate2D parseAsCoordinate(String coordinateDetails) {
+        coordinateDetails = coordinateDetails.trim();
+        try {
+            int commaIndex = coordinateDetails.indexOf(',');
+            int x = Integer.parseInt(coordinateDetails.substring(0, commaIndex));
+            int y = Integer.parseInt(coordinateDetails.substring(commaIndex+1));
+            return new Coordinate2D(x, y);
+        } catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("Coordinate is not in int,int format: " + coordinateDetails);
+        }
+    }
+
+    private static void addRocksToSet(Coordinate2D start, Coordinate2D end) {
+        if (start.equals(end)) {
+            rocksSet.add(start);
+            return;
+        }
+
+        if (start.getX() == end.getX() && start.getY() > end.getY()) {
+            addRocksInLoop(start, end, 0, -1);
+        } 
+        else if (start.getX() == end.getX() && start.getY() < end.getY()) {
+            addRocksInLoop(start, end, 0, 1);
+        } 
+        else if (start.getX() > end.getX() && start.getY() == end.getY()) {
+            addRocksInLoop(start, end, -1, 0);
+        }
+        else if (start.getX() < end.getX() && start.getY() == end.getY()) {
+            addRocksInLoop(start, end, 1, 0);
+        }
+        else {
+            throw new IllegalArgumentException("Input has diagonal lines: " + start + " --> " + end);
+        }
+    }
+    
+    private static void addRocksInLoop(Coordinate2D start, Coordinate2D end, int xOffset, int yOffset) {
+        Coordinate2D current = start;
+        while (!current.equals(end)) {
+            rocksSet.add(current);
+            current = new Coordinate2D(current.getX() + xOffset, current.getY() + yOffset);
+        }
+        rocksSet.add(current);
+    }
+}
--- advent_of_code_2022/problem_14/part1/SimulateSand.java
+++ advent_of_code_2022/problem_14/part1/SimulateSand.java
@@ -8,138 +8,73 @@ import advent_of_code_2022.common.ParseInput;
 public class SimulateSand {
     public static final String INPUT_FILE_NAME = "advent_of_code_2022/problem_14/input.txt";
     public static final Coordinate2D SAND_RESIVOIR = new Coordinate2D(500, 0);
+    private static final Coordinate2D INVALID_COORDINATE = new Coordinate2D(-1, -1);
+    
     private static final Coordinate2D[] FALLING_DIRECTION = new Coordinate2D[]{
         new Coordinate2D(0, 1),
         new Coordinate2D(-1, 1),
         new Coordinate2D(1, 1)
     };
+    private static HashSet<Coordinate2D> blockedSpaces;
 
     public static void main(String[] args) {
         String[] input = ParseInput.parseInputAsList(INPUT_FILE_NAME);
+
         System.out.println(countRestingSand(input) + " pieces of sand come to a rest before falling to the void.");
     }
 
-    /**
-     * Counts the units of sand that come to rest when falling down a rock formation.
-     * This is done by using a set to identify all of the locations of the rocks and finding the size.
-     * Then the sand pieces are placed into the set (as once they're at rest, they no longer move).
-     * The resulting pieces of sand at at rest is the difference of the two sizes.
-     * 
-     * @param input The locations of the rocks
-     * @return The number of units of sand that come to a rest on top of the rocks.
-     */
     public static int countRestingSand(String[] input) {
-        HashSet<Coordinate2D> blocked = new HashSet<Coordinate2D>();
-        // Parse through input to find all rocks
-        for (String line : input) {
-            addRocksToSet(blocked, line);
-        }
-        int totalRocks = blocked.size();
-        // For some reason, a high y value means a low point in space -- axis is reversed
-        int highestPoint = findHighestYValue(blocked);
+        blockedSpaces = ParseInputIntoRocks.getRocks(input);
+        int totalRocks = blockedSpaces.size();
+                
+        simulateSand();
+        int sandAndRocks = blockedSpaces.size();
 
-        // Simulates sand trickling down the rock formation
-        simulateSand(blocked, highestPoint);
-        int sandAndRocks = blocked.size();
         return sandAndRocks - totalRocks;
     }
 
-    /**
-     * Takes in Coordinates separated by -> that represent horizontal or vertical lines. Adds all coordinates in those lines to the set
-     * 
-     * @param blocked The set to be updated. Each coordinate in there represents a blocked space.
-     * @param line A string of Coordinates separated by ->
-     */
-    private static void addRocksToSet(HashSet<Coordinate2D> blocked, String line) {
-        if (line.isEmpty()) return;
-        String[] lineCoordinates = line.split("->");
-        if (lineCoordinates.length == 0) return;
-        
-        // Parse through input to find all rocks
-        Coordinate2D start = getCoordinate(lineCoordinates[0]);
-        blocked.add(start);
-        for (String coordinateDetails : lineCoordinates) {
-            Coordinate2D next = getCoordinate(coordinateDetails);
-            Coordinate2D difference = next.subtract(start);
-            int deltaX = difference.getX() == 0 ? 0 : difference.getX() / Math.abs(difference.getX());
-            int deltaY = difference.getY() == 0 ? 0 : difference.getY() /  Math.abs(difference.getY());
-
-            for (int diffX = 0; diffX != difference.getX(); diffX += deltaX) {
-                blocked.add(new Coordinate2D(start.getX() + diffX, start.getY()));
-            }
-            for (int diffY = 0; diffY != difference.getY(); diffY += deltaY) {
-                blocked.add(new Coordinate2D(start.getX(), start.getY() + diffY));
-            }
-            blocked.add(next);
-            start = next;
+    
+    private static void simulateSand() {
+        Coordinate2D nextSandLocation = findNextRestingSpot();
+        while (!nextSandLocation.equals(INVALID_COORDINATE)) {
+            blockedSpaces.add(nextSandLocation);
+            nextSandLocation = findNextRestingSpot();
         }
     }
-
-    /**
-     * Simulates all of the sand pieces trickling down the rocks formation. Stops once a piece of sand has no resting spot.
-     * 
-     * @param blocked The locations of the rocks.
-     * @param highestY An integer representing the lowest point of a rock. A sand below this point is considered to be falling forever.
-     */
-    private static void simulateSand(HashSet<Coordinate2D> blocked, int highestY) {
-        int blockedSize = blocked.size();
-        do {
-            blockedSize = blocked.size();
-            Coordinate2D nextSandLocation = findNextSandLocation(blocked, highestY);
-            if (nextSandLocation != null) blocked.add(nextSandLocation);
-        } while (blockedSize != blocked.size());
-    }
-
-    /**
-     * Returns the resulting Coordinate of the resting spot for the next piece of sand.
-     * Returns null if the sand is not caught by any rocks or sand, and falls to the void.
-     * 
-     * @param blocked A set containing all the locations of the rocks and sand. We assume these pieces cannot move.
-     * @param highestYCoord An integer indicating the lowest point of a rock. A sand below this point is effectively falling into the void.
-     * @return A Coordinate2D of the resting spot of the next sand piece. Returns null if it will fall forever.
-     */
-    private static Coordinate2D findNextSandLocation(HashSet<Coordinate2D> blocked, int highestYCoord) {
+    
+    
+    private static Coordinate2D findNextRestingSpot() {
         Coordinate2D sandParticle = SAND_RESIVOIR;
-        while (sandParticle.getY() <= highestYCoord) {
-            Coordinate2D next = null;
-            boolean foundNextSpot = false;
-            // Check straight down, down left, and then down right for a valid location
-            for (Coordinate2D direction : FALLING_DIRECTION) {
-                if (!blocked.contains(sandParticle.add(direction))) {
-                    next = sandParticle.add(direction);
-                    foundNextSpot = true;
-                    break;
-                }
+        int lowestElevation = findLowestBlockedPoint();
+
+        // Once again, note that a low coordinate has a high y value
+        while (sandParticle.getY() <= lowestElevation) {
+            Coordinate2D next = findNextFallingFame(sandParticle);
+            
+            if (next.equals(sandParticle)) {
+                // Stayed in place
+                return sandParticle;
             }
-            if (foundNextSpot) {
-                sandParticle = next;
-            }
-            else return sandParticle;
+            sandParticle = next;
         }
-        // We past all the rocks, now there's just the void left
-        return null;
+        return INVALID_COORDINATE;
+    }
+    
+    private static int findLowestBlockedPoint() {
+        int lowest = 0;
+        for (Coordinate2D coordinate : blockedSpaces) {
+            // Note that a low coordinate has a high y value
+            lowest = Math.max(lowest, coordinate.getY());
+        }
+        return lowest;
     }
 
-    // Loops through the set and returns the highest Y coordinate
-    // Keep in mind that for some reason, a higher y coordinate means a lower point in space
-    private static int findHighestYValue(HashSet<Coordinate2D> blocked) {
-        int highest = 0;
-        for (Coordinate2D coordinate : blocked) {
-            highest = Math.max(highest, coordinate.getY());
+    private static Coordinate2D findNextFallingFame(Coordinate2D sandParticle) {
+        for (Coordinate2D direction : FALLING_DIRECTION) {
+            if (!blockedSpaces.contains(sandParticle.add(direction))) {
+                return sandParticle.add(direction);
+            }
         }
-        return highest;
-    }
-
-    // Returns a coordinate from a ABC,DE format string
-    private static Coordinate2D getCoordinate(String coordinateDetails) {
-        coordinateDetails = coordinateDetails.trim();
-        try {
-            int commaIndex = coordinateDetails.indexOf(',');
-            int x = Integer.parseInt(coordinateDetails.substring(0, commaIndex));
-            int y = Integer.parseInt(coordinateDetails.substring(commaIndex+1));
-            return new Coordinate2D(x, y);
-        } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("Coordinate is not in int,int format: " + coordinateDetails);
-        }
+        return sandParticle;
     }
 }
